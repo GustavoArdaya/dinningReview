2f4605cd752e5d0caf769d1773ab92fbc73ea7c7
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-23 00:10:11 +0200

    add Review entity

--- src/main/java/com/codecademy/dinningReview/model/Review.java
+++ src/main/java/com/codecademy/dinningReview/model/Review.java
@@ -1,8 +1,23 @@
 package com.codecademy.dinningReview.model;
 
+import jakarta.persistence.*;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Review {
-    private List<Rating> ratingList = new ArrayList<>();
+    //private List<Rating> ratingList = new ArrayList<>();
+    private Long id;
+    private String author;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    private Rating peanutRating;
+    private Rating eggRating;
+
+    private Rating dairyRating;
+
+    private String commentary;
 }
