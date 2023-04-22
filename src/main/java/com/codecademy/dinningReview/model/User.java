81ef9e168f3cfa578ec7dab6273d1a7a4662ba3c
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-23 00:15:32 +0200

    add User entity

---
+++ src/main/java/com/codecademy/dinningReview/model/User.java
@@ -0,0 +1,22 @@
+package com.codecademy.dinningReview.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String city;
+    private String state;
+    private String zipCode;
+    private Boolean interestedInPeanutAllergies;
+    private Boolean interestedInEggAllergies;
+    private Boolean interestedInDairyAllergies;
+
+}
